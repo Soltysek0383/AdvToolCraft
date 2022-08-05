ff15d6ec293fa98cf945008e37d0c14a8047aa17
pozis <pozisko@gmail.com>
2022-08-06 01:53:24 +0200

    0.0.1

--- src/main/java/net/SaberWolf/advtoolcraft/Blocks/AddBlocks.java
+++ src/main/java/net/SaberWolf/advtoolcraft/Blocks/AddBlocks.java
@@ -14,11 +14,10 @@ import net.minecraft.util.registry.Registry;
 
 public class AddBlocks {
     
-    public static final Block IRON_VAIN = registerblock("iron_vain", new Block(FabricBlockSettings.of(Material.STONE).strength(4.0f)), AdvToolCraft.ADV_CRAFT);
-    //public static final Block COAL_VAIN = registerblock("coal_vain", new Block(new FabricBlockSettings().group(AdvToolCraft.ADV_CRAFT)));
-    //public static final Block GOLD_VAIN = registerblock("gold_vain", new Block(new FabricBlockSettings().group(AdvToolCraft.ADV_CRAFT)));
-    //public static final Block COPPER_VAIN = registerblock("copper_vain", new Block(new FabricBlockSettings().group(AdvToolCraft.ADV_CRAFT)));
-
+    public static final Block IRON_VAIN = registerblock("iron_vain", new Block(FabricBlockSettings.of(Material.STONE).strength(4.0f).luminance(3)), AdvToolCraft.ADV_CRAFT);
+    public static final Block GOLD_VAIN = registerblock("gold_vain", new Block(FabricBlockSettings.of(Material.STONE).strength(4.0f)), AdvToolCraft.ADV_CRAFT);
+    public static final Block COPPER_VAIN = registerblock("copper_vain", new Block(FabricBlockSettings.of(Material.STONE).strength(4.0f)), AdvToolCraft.ADV_CRAFT);
+    public static final Block COAL_VAIN = registerblock("coal_vain", new Block(FabricBlockSettings.of(Material.STONE).strength(4.0f)), AdvToolCraft.ADV_CRAFT);
     private static Block registerblock(String name, Block block , ItemGroup group){
         registerblockitem(name , block , group);
         return Registry.register(Registry.BLOCK, new Identifier(AdvToolCraft.Mod_ID,name), block);
